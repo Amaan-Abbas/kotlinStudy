3b039a73d6c3435bac397012708b365364085f60
Amaan Abbas <amaan140904@gmail.com>
2024-09-19 19:34:43 +0530

    First Kotlin file

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+var number = 20
+fun main() {
+    //var number: Int = 10
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+    //println(number)
+
+    number = 10
+
+    println("Hello, $number!")
+
+}
