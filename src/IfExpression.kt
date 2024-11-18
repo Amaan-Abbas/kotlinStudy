66333987a32b860ba5f65e8e618d625770cfd9d8
Amaan Abbas <amaan140904@gmail.com>
2024-11-18 15:43:23 +0530

    IF ELSE in ezpression form

--- src/IfExpression.kt
+++ src/IfExpression.kt
@@ -1,12 +1,14 @@
 fun main() {
     val number = 10
 
-    val resultExpression = if (number % 2 == 0) {
-        "$number is an even number!"
-    }
-    else {
-        "$number is an odd number!"
-    }
+//    val resultExpression = if (number % 2 == 0) {
+//        "$number is an even number!"
+//    }
+//    else {
+//        "$number is an odd number!"
+//    }
 
+
+    val resultExpression = if (number % 2 == 0) "$number is an even number!" else "$number is an odd number!"
     println(resultExpression)
 }
