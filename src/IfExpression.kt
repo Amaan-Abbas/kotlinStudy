862086d504b41eee5040c1ae7a3bc2f9f869deae
Amaan Abbas <amaan140904@gmail.com>
2024-11-18 15:34:49 +0530

    If Else in expression form

---
+++ src/IfExpression.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val number = 10
+
+    val resultExpression = if (number % 2 == 0) {
+        "$number is an even number!"
+    }
+    else {
+        "$number is an odd number!"
+    }
+
+    println(resultExpression)
+}
