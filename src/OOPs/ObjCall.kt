cb934501bf62ab0d709d42b1d0985bf5295ad00f
Amaan Abbas <amaan140904@gmail.com>
2024-12-03 23:58:41 +0530

    Access modifiers

---
+++ src/OOPs/ObjCall.kt
@@ -0,0 +1,47 @@
+package OOPs
+
+import java.util.Scanner
+
+open class Parent(private val name: String) {
+    fun admissionStudent() {
+        var num = 0
+        for (i in 1..10) {
+            if (i%2 == 0) {
+                num += i
+            }
+        }
+        println("The sum of even numbers is: $num")
+    }
+
+    open fun display() {
+        println("The name of the student is: $name")
+    }
+}
+class ObjCall(name: String): Parent(name) {
+    private var number = 10
+        set(value) {
+            if (value in 1..10) {
+                field = value
+            }
+        }
+
+    private fun sum() {
+        println("the number value of the number is: $number")
+    }
+    override fun display() {
+        super.display()
+        sum()
+    }
+}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    print("Enter the name of the student: ")
+    val name = sc.next()
+
+    val obj = ObjCall(name = name)
+
+    obj.display()
+    obj.admissionStudent()
+}
