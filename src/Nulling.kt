5dad71df021e93fd9497c02d1fc86b9dfd47e250
Amaan Abbas <amaan140904@gmail.com>
2024-12-31 19:18:51 +0530

    Handling null variables using ?. and !!. and if-else

--- src/Nulling.kt
+++ src/Nulling.kt
@@ -8,4 +8,6 @@ fun main() {
     println(name)
 
     if (name != null) println("Hello") else println("Hi")
+
+    println("The length of the the string is: ${name!!.length}") // the reference is to that length function returns the length of the string while asserting th value of the string is nor null even if it is actually  null.
 }
